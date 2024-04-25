2d7539f858a44183b51bea68315fb91c68c16c07
Abu Darda <abu.darda@brainstation-23.com>
2024-04-25 12:08:12 +0600

    feat: factorial can calculate fibonacci as well now

--- factorial.cpp
+++ factorial.cpp
@@ -16,6 +16,12 @@ int factorial(int n){
     return n * factorial(n-1);
 }
 
+int fibonacci(int n){
+    if (n == 0 or n == 1) return 1;
+    return fibonacci(n-2) + fibonacci(n-1);
+}
+
 int main(){
     cout << factorial(5) << endl;
+    cout << fibonacci(5) << endl;
 }
