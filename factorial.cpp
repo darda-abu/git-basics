7647643de3ea669fcdd2530d7dd878ba3f031045
Abu Darda <abu.darda@brainstation-23.com>
2024-04-25 13:26:49 +0600

    commit after squash merge

--- factorial.cpp
+++ factorial.cpp
@@ -35,6 +35,8 @@ int fibonacci(int n){
     if (n == 0 or n == 1) return 1;
     return fibonacci(n-2) + fibonacci(n-1);
 }
+//squash line 1
+//squash line 2
 
 void criticalFix(){
     cout<<"fixed"<<endl;
