5c73e831ef48720db8283c4b0e6ff2c945d87802
Abu Darda <abu.darda@brainstation-23.com>
2024-04-25 12:25:06 +0600

    added critical fix to factorial problem

--- factorial.cpp
+++ factorial.cpp
@@ -15,7 +15,9 @@ int factorial(int n){
     if (n == 0) return 1;
     return n * factorial(n-1);
 }
-
+void criticalFix(){
+    cout<<"fixed"<<endl;
+}
 int main(){
     cout << factorial(5) << endl;
 }
