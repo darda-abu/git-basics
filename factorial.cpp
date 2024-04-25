4de5d29e440708bffc66fff2212525699044ebf9
Abu Darda <abu.darda@brainstation-23.com>
2024-04-25 11:55:55 +0600

    feat: added factorial in feature-branch

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define LL              long long int
+#define PB              push_back    
+#define MP              make_pair    
+#define pii             pair<int,int>
+#define endl            "\n"   
+#define F               first  
+#define S               second 
+#define inf             INT_MAX
+using namespace std;  
+const int nmax=2e6+69;
+const LL M=1e9+7;     
+
+int factorial(int n){
+    if (n == 0) return 1;
+    return n * factorial(n-1);
+}
+
+int main(){
+    cout << factorial(5) << endl;
+}
