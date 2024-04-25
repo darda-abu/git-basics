47c5ead0fceb8085df2028b7ee6a5fa372113d69
Abu Darda <abu.darda@brainstation-23.com>
2024-04-25 12:29:10 +0600

    addidng isPrime function

--- factorial.cpp
+++ factorial.cpp
@@ -11,6 +11,19 @@ using namespace std;
 const int nmax=2e6+69;
 const LL M=1e9+7;     
 
+int isPrime(int k){
+    if (k <= 1)
+        return 0;
+    if (k==2 || k==3)
+        return 1;
+    if (k % 2 == 0 || k % 3 == 0)
+        return 0;
+    for (int i = 5; i * i <= k; i = i + 6)
+        if (k % i == 0 || k % (i + 2) == 0)
+            return 0;
+    return 1;
+}
+ 
 int factorial(int n){
     if (n == 0) return 1;
     return n * factorial(n-1);
